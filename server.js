ad7d44fdda4df382d247180ce1b423eebaf0bdc5
reachtoshine <reachtoshine@outlook.com>
2024-12-26 14:16:26 +0900

    delete_추가

--- server.js
+++ server.js
@@ -75,3 +75,8 @@ app.put('/edit', async (요청, 응답)=>{
   })
   응답.redirect('/list')
 }) 
+
+app.delete('/delete', async (요청, 응답) => {
+  let result = await db.collection('posts').deleteOne( { _id : new ObjectId(요청.query.docid) } )
+  응답.send('삭제완료')
+})
