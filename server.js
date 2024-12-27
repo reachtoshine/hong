8456866a47dbf7e832f595f2b6e6b6060678185e
reachtoshine <reachtoshine@outlook.com>
2024-12-27 15:25:57 +0900

    callbackurl_serverjs_와_auth.ejs_싱크_진짜로-맞춤

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(session({secret: 'G5gXGAkMZb', resave: false, saveUninitialized: true}))
 passport.use(new NaverStrategy({
   clientID: 'yqpW_K1jOFEsdiOFzuab',
   clientSecret: 'G5gXGAkMZb',
-  callbackURL: '/aurh/naver',
+  callbackURL: '/auth/naver',
 }, (accessToken, refreshToken, profile, done) => {
   return done(null, profile);
 }));
