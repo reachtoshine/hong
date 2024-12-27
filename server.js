04379d723bdb47d36b1d629f6be5e70a3ec6587e
reachtoshine <reachtoshine@outlook.com>
2024-12-27 14:51:33 +0900

    exprerimental_naver_login_added

--- server.js
+++ server.js
@@ -3,12 +3,32 @@ const methodOverride = require('method-override')
 const app = express()
 const { MongoClient } = require('mongodb')
 const { ObjectId } = require('mongodb') 
+const session = require('express-session');
+const passport = require('passport');
+const NaverStrategy = require('passport-naver').Strategy;
 
 app.set('view engine', 'ejs') 
 app.use(express.json())
 app.use(express.urlencoded({extended:true})) 
 app.use(express.static(__dirname + '/public'));
 app.use(methodOverride('_method')) 
+app.use(session({secret: 'G5gXGAkMZb', resave: false, saveUninitialized: true}));
+
+
+passport.use(new NaverStrategy({
+  clientID: 'yqpW_K1jOFEsdiOFzuab',
+  clientSecret: 'G5gXGAkMZb',
+  callbackURL: '/login/naver',
+}, (accessToken, refreshToken, profile, done) => {
+  return done(null, profile);
+}));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
 
 let db
 const url = 'mongodb+srv://reachtoshine:Ov79bA0M9DU8YQq8@cluster0.lvgghli.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
@@ -77,3 +97,32 @@ app.delete('/delete', async (요청, 응답) => {
   let result = await db.collection('posts').deleteOne( { _id : new ObjectId(요청.query.docid) } )
   응답.send('삭제완료')
 })
+
+
+// 로그인 라우터
+app.get('/auth/naver', passport.authenticate('naver'));
+
+// 콜백 라우터
+app.get('/auth/naver/callback',
+  passport.authenticate('naver', { failureRedirect: '/' }),
+  (req, res) => {
+    // 성공적으로 로그인한 후
+    res.redirect('/profile');
+  }
+);
+
+// 프로필 페이지
+app.get('/profile', (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.redirect('/');
+  }
+  res.json(req.user);
+});
+
+// 로그아웃
+app.get('/logout', (req, res) => {
+  req.logout(err => {
+    if (err) return next(err);
+    res.redirect('/');
+  });
+});
